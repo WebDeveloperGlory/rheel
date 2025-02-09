c319936fc842f87a588459e930106bfa42fb8ef2
OgheneDev <emmanueloghene72@gmail.com>
2025-02-09 08:32:18 +0100

    Format prices

--- src/components/properties/PropertyCard.jsx
+++ src/components/properties/PropertyCard.jsx
@@ -6,8 +6,14 @@ const PropertyCard = ({ property, onEdit, onDelete }) => {
   const [showActions, setShowActions] = useState(false);
   const navigate = useNavigate();
 
+  const formatPrice = (price) => {
+    return new Intl.NumberFormat('en-NG', {
+      maximumFractionDigits: 0
+    }).format(price);
+  };
+
   const handleImageClick = (e) => {
-    e.stopPropagation(); // Prevent event bubbling
+    e.stopPropagation();
     navigate(`/properties/${property.id}`);
   };
 
@@ -24,7 +30,7 @@ const PropertyCard = ({ property, onEdit, onDelete }) => {
 
       <div className="p-3 ">
         <h1 className='text-[#181818] font-bold text-xl mb-2'>Property Name</h1>
-        <h3 className="text-[15px]  text-[#FF5B19] mb-2">₦ { property.price }</h3>
+        <h3 className="text-[15px]  text-[#FF5B19] mb-2">₦ {formatPrice(property.price)}</h3>
         <div className="flex items-center gap-2 mb-3">
           <MapPin className="w-5 h-5 text-[#7F7F7F]" />
           <span className="text-[#7F7F7F] text-[14px]">{ property.location }</span>
